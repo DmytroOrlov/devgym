9560152ec30f03a3061026f116ad08b6c8adb94c
Dmytro Orlov <orlov.dm@gmail.com>
2016-01-23 21:51:56 +0100

    runTest: misc

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,7 +8,7 @@ import play.api.data.validation.{Constraint, Invalid, Valid, ValidationError}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 
-import scala.concurrent.Future
+import scala.concurrent.{Promise, Future}
 
 class Application @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
@@ -16,10 +16,6 @@ class Application @Inject()(val messagesApi: MessagesApi) extends Controller wit
     mapping(
       "prob" -> nonEmptyAndChanged(original = blank)
     )(ProbForm.apply)(ProbForm.unapply)
-  }
-
-  def nonEmptyAndChanged(original: String) = nonEmptyText verifying Constraint[String]("changes.required") { o =>
-    if (o.filter(_ != '\r') == original) Invalid(ValidationError("error.changesRequired")) else Valid
   }
 
   def index = Action(Redirect(routes.Application.getProb))
@@ -36,7 +32,11 @@ class Application @Inject()(val messagesApi: MessagesApi) extends Controller wit
       })
   }
 
-  def runTest(answerCode: String): Future[Result] = ???
+  def runTest(answerCode: String): Future[Result] = {
+    val p = Promise[Result]()
+
+    p.future
+  }
 }
 
 case class ProbForm(prob: String)
@@ -45,4 +45,8 @@ object Application {
   val task = "The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.\n\nsleepIn(false, false) → true\nsleepIn(true, false) → false\nsleepIn(false, true) → true"
 
   val blank = "public boolean sleepIn(boolean weekday, boolean vacation) {\n  \n}"
+
+  def nonEmptyAndChanged(original: String) = nonEmptyText verifying Constraint[String]("changes.required") { o =>
+    if (o.filter(_ != '\r') == original) Invalid(ValidationError("error.changesRequired")) else Valid
+  }
 }
