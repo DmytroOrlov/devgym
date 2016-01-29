c41e4549de57913e8110f3497e9cc08072ba171d
Dmytro Orlov <orlov.dm@gmail.com>
2016-01-29 14:28:18 +0100

    cassandra: change to month

--- app/dal/Repo.scala
+++ app/dal/Repo.scala
@@ -1,5 +1,7 @@
 package dal
 
+import java.time.temporal.ChronoUnit
+import java.time.{ZoneOffset, ZonedDateTime}
 import java.util.Date
 
 import com.datastax.driver.core.Session
@@ -17,20 +19,17 @@ class Repo @Inject()(cluster: CassandraCluster)(implicit ec: ExecutionContext) {
       " VALUES (?, ?, NOW()) IF NOT EXISTS")
 
   protected lazy val addClassSolutionStatement = session.prepare(
-    "INSERT INTO solution (type, week, task, blank, test)" +
-      " VALUES (?, ?, ?, ?, ?)")
+    "INSERT INTO solution (type, month, uuid, task, blank, test)" +
+      " VALUES (?, ?, NOW(), ?, ?, ?)")
 
   def create(user: User) = toFuture(session.executeAsync(createUserStatement.bind(user.name, user.password)))
 
-  def addSolution(prob: Prob) = {
-    val week = new Date({
-      val nowMillis = System.currentTimeMillis()
-      nowMillis - nowMillis % oneWeekMillis
-    })
-    toFutureUnit(session.executeAsync(addClassSolutionStatement.bind(prob.`class`, week, prob.task, prob.blank, prob.test)))
-  }
+  def addSolution(prob: Prob) = toFutureUnit(
+    session.executeAsync(addClassSolutionStatement.bind(prob.`class`, month, prob.task, prob.blank, prob.test))
+  )
 }
 
 object Repo {
-  val oneWeekMillis = 7 * 24 * 60 * 60 * 1000
+  def month =
+    Date.from(ZonedDateTime.now(ZoneOffset.UTC).truncatedTo(ChronoUnit.DAYS).withDayOfMonth(1).toInstant)
 }
