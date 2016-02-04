1d5f63a6ac994093e7bd357eb6386fa58c5a8ff2
Dmytro Orlov <orlov.dm@gmail.com>
2016-02-04 04:14:24 +0100

    test: add ApplicationTest

---
+++ server/test/controllers/application/ApplicationTest.scala
@@ -0,0 +1,39 @@
+package controllers.application
+
+import org.scalatestplus.play._
+import play.api.test.Helpers._
+import play.api.test._
+
+class ApplicationTest extends PlaySpec with OneAppPerSuite {
+
+  "Application" when {
+    "get root" should {
+      "result with index" in {
+        val Some(result) = route(FakeRequest(GET, "/"))
+        status(result) mustBe OK
+      }
+    }
+    "get logout" should {
+      "redirect" in {
+        val Some(result) = route(FakeRequest(GET, "/logout"))
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result) mustBe Some("/")
+      }
+    }
+  }
+  "NewTask" when {
+    "get addTask" should {
+      "result with OK" in {
+        val Some(result) = route(FakeRequest(GET, "/addTask"))
+        status(result) mustBe OK
+      }
+    }
+    "post no form to addTask" should {
+      "result BadRequest with form" in {
+        val Some(result) = route(FakeRequest(POST, "/addTask"))
+        status(result) mustBe BAD_REQUEST
+        contentAsString(result) must (include("<form") and include("This field is required"))
+      }
+    }
+  }
+}
