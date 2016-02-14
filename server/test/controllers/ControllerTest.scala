72414ab1ef99433a1835a908d5d23d3a67e991cd
novakov_alex <novakov.alex@gmail.com>
2016-02-14 11:15:47 -0500

    Cover with Unit Tests new classes: existing test formatting

--- server/test/controllers/ControllerTest.scala
+++ server/test/controllers/ControllerTest.scala
@@ -21,10 +21,13 @@ class ControllerTest extends PlaySpec with MockFactory {
   "AddTask controller" when {
     "post fail with scalaTestRunner when addTask" should {
       "result BadRequest with error" in {
+        //given
         val scalaTestRunner = mock[DynamicSuiteExecutor]
         (scalaTestRunner.apply(_: String, _: String)(_: String => Unit)(_: Scheduler)) expects("4", "5", *, *)
+        //when
         withAddTaskController(scalaTestRunner, null)({ controller =>
           val result = controller.postNewTask(FakeRequest("POST", "ignore").withFormUrlEncodedBody("taskName" -> "1", "taskDescription" -> "2", "solutionTemplate" -> "3", "referenceSolution" -> "4", "suite" -> "5"))
+          //then
           status(result) mustBe BAD_REQUEST
           contentAsString(result) must include( """class="error"></dd>""")
         })
@@ -32,12 +35,15 @@ class ControllerTest extends PlaySpec with MockFactory {
     }
     "post fail with dao when addTask" should {
       "result BadRequest with error" in {
+        //given
         val scalaTestRunner = mock[DynamicSuiteExecutor]
         (scalaTestRunner.apply(_: String, _: String)(_: String => Unit)(_: Scheduler)) expects("4", "5", *, *)
         val dao = mock[Dao]
         dao.addTask _ expects NewTask(scalaClass, "1", "2", "3", "4", "5") returns Future.failed(new RuntimeException)
+        //when
         withAddTaskController(scalaTestRunner, dao)({ controller =>
           val result = controller.postNewTask(FakeRequest("POST", "ignore").withFormUrlEncodedBody("taskName" -> "1", "taskDescription" -> "2", "solutionTemplate" -> "3", "referenceSolution" -> "4", "suite" -> "5"))
+          //then
           status(result) mustBe INTERNAL_SERVER_ERROR
           contentAsString(result) must include( """class="error"></dd>""")
         })
@@ -45,12 +51,15 @@ class ControllerTest extends PlaySpec with MockFactory {
     }
     "post correct addTask" should {
       "persist and redirect" in {
+        //given
         val scalaTestRunner = mock[DynamicSuiteExecutor]
         (scalaTestRunner.apply(_: String, _: String)(_: String => Unit)(_: Scheduler)) expects("4", "5", *, *)
         val dao = mock[Dao]
         dao.addTask _ expects NewTask(scalaClass, "1", "2", "3", "4", "5") returns Future.successful(())
+        //when
         withAddTaskController(scalaTestRunner, dao)({ controller =>
           val result = controller.postNewTask(FakeRequest("POST", "ignore").withFormUrlEncodedBody("taskName" -> "1", "taskDescription" -> "2", "solutionTemplate" -> "3", "referenceSolution" -> "4", "suite" -> "5"))
+          //then
           status(result) mustBe SEE_OTHER
           redirectLocation(result) mustBe Some("/")
         })
@@ -58,19 +67,14 @@ class ControllerTest extends PlaySpec with MockFactory {
     }
     "post FAILED addTask" should {
       "fall in ScalaTestRunner" in {
+        //given
         val solution = "class SubArrayWithMaxSum {\n  def apply(a: Array[Int]): Array[Int] = {\n    var currentSum = 0\n    var maxSum = 0\n    var left, right = 0\n    var maxI = 0 //used when all negatives in the array\n\n    for (i <- a.indices) {\n      val incSum = currentSum + a(i)\n\n      if (incSum > 0) {\n        currentSum = incSum\n\n        if (currentSum > maxSum) {\n          maxSum = currentSum\n          right = i\n        }\n      } else {\n        left = i + 1\n        right = left\n        currentSum = 0\n        if (a(i) > a(maxI)) maxI = i\n      }\n    }\n\n    if (left == a.length) a.slice(maxI, maxI + 1)\n    else a.slice(left, right + 1)\n  }\n}"
         val suite = "import org.scalatest.{FlatSpec, Matchers}\n\nclass SubArrayWithMaxSumTest(solution: SubArrayWithMaxSumSolution) extends FlatSpec with Matchers {\n  behavior of \"SubArrayWithMaxSum\"\n\n  it should \"return max sum sub array within given array\" in {\n    solution.apply(Array(-2, 1, -3, 4, -1, 2, 1, -5, 4)) should be(Array(4, -1, 2, 1))\n    solution.apply(Array(-2, 1, -3, 4, -1, 2, 1, 5, 4)) should be(Array(4, -1, 2, 1, 5, 4))\n    solution.apply(Array(2, -1, 0, 0, 0, 0, 1)) should be(Array(2))\n  }\n\n  it should \"return the whole array when given array has only positive numbers\" in {\n    solution.apply(Array(2, 1, 3, 4, 1, 2, 1, 5, 4)) should be(Array(2, 1, 3, 4, 1, 2, 1, 5, 4))\n  }\n\n  it should \"return max sum sub array when given array contains only negative numbers\" in {\n    solution.apply(Array(-2, -1, -3, -4, -1, -2, -1, -5, -4)) should be(Array(-1))\n    solution.apply(Array(-2, -3, -3, -4, -6, -2, -6, -5, -1)) should be(Array(-2))\n  }\n}\n\ntrait SubArrayWithMaxSumSolution {\n  def apply(a: Array[Int]): Array[Int]\n}"
-        val dao = new Dao {
-          def addTask(task: NewTask) = Future.successful(())
-
-          def create(user: User) = ???
-
-          def getTasks(`type`: TaskType, limit: Port, yearAgo: Port) = ???
-
-          def getTask(year: Date, taskType: TaskType, timeuuid: UUID) = ???
-        }
+        val dao = mock[Dao]
+        //when
         withAddTaskController(new ScalaTestRunner, dao)({ controller =>
           val result = controller.postNewTask(FakeRequest("POST", "ignore").withFormUrlEncodedBody("taskDescription" -> "1", "solutionTemplate" -> "2", "referenceSolution" -> solution, "suite" -> suite))
+          //then
           status(result) mustBe BAD_REQUEST
           contentAsString(result) must include( """class="error"></dd>""")
         })
--- server/test/controllers/application/ApplicationTest.scala
+++ server/test/controllers/application/ApplicationTest.scala
@@ -26,41 +26,60 @@ class ApplicationTest extends PlaySpec with OneAppPerSuite {
     "get addTask" should {
       "result with form" in {
         val Some(result) = route(FakeRequest(GET, "/addTask"))
+
         status(result) mustBe OK
-        contentAsString(result) must (include("<form") and include("/addTask") and include("taskName") and include("taskDescription") and include("solutionTemplate") and include("referenceSolution") and include("suite") and include("suite") and include( """<input type="submit""""))
+        contentAsString(result) must (
+          include("<form") and include("/addTask") and include("taskName")
+          and include("taskDescription") and include("solutionTemplate") and include("referenceSolution")
+          and include("suite") and include("suite") and include( """<input type="submit""""))
       }
     }
     "post form with missed fields to addTask" should {
       "result BadRequest with error1" in {
         val Some(result) = route(FakeRequest(POST, "/addTask").withFormUrlEncodedBody("taskName" -> "0", "taskDescription" -> "1", "solutionTemplate" -> "2", "referenceSolution" -> "3" /*, "suite" -> "4"*/))
+
         status(result) mustBe BAD_REQUEST
-        contentAsString(result) must (include("<form") and include("/addTask") and include("taskDescription") and include("solutionTemplate") and include("referenceSolution") and include("suite") and include("suite") and include( """<input type="submit""""))
+        contentAsString(result) must (
+          include("<form") and include("/addTask") and include("taskDescription") and include("solutionTemplate")
+            and include("referenceSolution") and include("suite") and include("suite") and include( """<input type="submit""""))
         contentAsString(result) must (include( """class="error"""") and include("This field is required"))
       }
       "result BadRequest with error2" in {
         val Some(result) = route(FakeRequest(POST, "/addTask").withFormUrlEncodedBody("taskName" -> "0", "taskDescription" -> "1", "solutionTemplate" -> "2", /*"referenceSolution" -> "3",*/ "suite" -> "4"))
+
         status(result) mustBe BAD_REQUEST
-        contentAsString(result) must (include("<form") and include("/addTask") and include("taskDescription") and include("solutionTemplate") and include("referenceSolution") and include("suite") and include("suite") and include( """<input type="submit""""))
+        contentAsString(result) must (include("<form") and include("/addTask") and include("taskDescription")
+          and include("solutionTemplate") and include("referenceSolution") and include("suite") and include("suite")
+          and include( """<input type="submit""""))
         contentAsString(result) must (include( """class="error"""") and include("This field is required"))
       }
       "result BadRequest with error3" in {
         val Some(result) = route(FakeRequest(POST, "/addTask").withFormUrlEncodedBody("taskName" -> "0", "taskDescription" -> "1", /*"solutionTemplate" -> "2",*/ "referenceSolution" -> "3", "suite" -> "4"))
+
         status(result) mustBe BAD_REQUEST
-        contentAsString(result) must (include("<form") and include("/addTask") and include("taskDescription") and include("solutionTemplate") and include("referenceSolution") and include("suite") and include("suite") and include( """<input type="submit""""))
+        contentAsString(result) must (include("<form") and include("/addTask") and include("taskDescription")
+          and include("solutionTemplate") and include("referenceSolution") and include("suite") and include("suite")
+          and include( """<input type="submit""""))
         contentAsString(result) must (include( """class="error"""") and include("This field is required"))
       }
       "result BadRequest with error4" in {
         val Some(result) = route(FakeRequest(POST, "/addTask").withFormUrlEncodedBody("taskName" -> "0", /*"taskDescription" -> "1",*/ "solutionTemplate" -> "2", "referenceSolution" -> "3" /*, "suite" -> "4"*/))
+
         status(result) mustBe BAD_REQUEST
-        contentAsString(result) must (include("<form") and include("/addTask") and include("taskDescription") and include("solutionTemplate") and include("referenceSolution") and include("suite") and include("suite") and include( """<input type="submit""""))
+        contentAsString(result) must (include("<form") and include("/addTask") and include("taskDescription")
+          and include("solutionTemplate") and include("referenceSolution") and include("suite") and include("suite")
+          and include( """<input type="submit""""))
         contentAsString(result) must (include( """class="error"""") and include("This field is required"))
       }
     }
     "post form with bad solution to addTask" should {
       "result BadRequest with error" in {
         val Some(result) = route(FakeRequest(POST, "/addTask").withFormUrlEncodedBody("taskName" -> "0", "taskDescription" -> "1", "solutionTemplate" -> "2", "referenceSolution" -> "3", "suite" -> "4"))
+
         status(result) mustBe BAD_REQUEST
-        contentAsString(result) must (include("<form") and include("/addTask") and include("taskDescription") and include("solutionTemplate") and include("referenceSolution") and include("suite") and include("suite") and include( """<input type="submit""""))
+        contentAsString(result) must (include("<form") and include("/addTask") and include("taskDescription")
+          and include("solutionTemplate") and include("referenceSolution") and include("suite") and include("suite")
+          and include( """<input type="submit""""))
         contentAsString(result) must (include( """class="error"""") and include("Can not add your task"))
       }
     }
