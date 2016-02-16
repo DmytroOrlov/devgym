506946128ee677fb553bcf731765d43b3102cc1c
novakov_alex <novakov.alex@gmail.com>
2016-02-15 20:48:14 -0500

    add tests: ApplicationTest.scala

--- server/test/controllers/application/ApplicationTest.scala
+++ server/test/controllers/application/ApplicationTest.scala
@@ -1,5 +1,8 @@
 package controllers.application
 
+import java.time.{LocalDate, ZoneOffset}
+import java.util.{Date, UUID}
+
 import org.scalatestplus.play._
 import play.api.test.Helpers._
 import play.api.test._
@@ -11,7 +14,8 @@ class ApplicationTest extends PlaySpec with OneAppPerSuite {
       "result with index" in {
         val Some(result) = route(FakeRequest(GET, "/"))
         status(result) mustBe OK
-        contentAsString(result) must (include("/task") and include("/addTask") and include("/register"))
+        contentAsString(result) must (include("/task") and include("/addTask") and include("/register")
+          and include( """<li><a href="/task/"""))
       }
     }
     "get logout" should {
@@ -30,8 +34,8 @@ class ApplicationTest extends PlaySpec with OneAppPerSuite {
         status(result) mustBe OK
         contentAsString(result) must (
           include("<form") and include("/addTask") and include("taskName")
-          and include("taskDescription") and include("solutionTemplate") and include("referenceSolution")
-          and include("suite") and include("suite") and include( """<input type="submit""""))
+            and include("taskDescription") and include("solutionTemplate") and include("referenceSolution")
+            and include("suite") and include("suite") and include( """<input type="submit""""))
       }
     }
     "post form with missed fields to addTask" should {
@@ -81,6 +85,30 @@ class ApplicationTest extends PlaySpec with OneAppPerSuite {
           and include("solutionTemplate") and include("referenceSolution") and include("suite") and include("suite")
           and include( """<input type="submit""""))
         contentAsString(result) must (include( """class="error"""") and include("Can not add your task"))
+      }
+    }
+  }
+
+  "TaskSolver" when {
+    "get the available task" should {
+      "result with form" in {
+        val year = LocalDate.of(2016, 1, 1)
+        val instant = year.atStartOfDay().atZone(ZoneOffset.UTC).toInstant
+
+        val Some(result) = route(FakeRequest(GET, s"/task/scalaClass/${Date.from(instant).getTime}/9894cd10-ce12-11e5-8ee9-091830ac5256"))
+
+        status(result) mustBe OK
+        contentAsString(result) must (
+          include("<form") and include("solution") and include("taskType") and include("timeuuid") and include("year")
+            and include( """<input type="button""""))
+      }
+    }
+    "get the unavailable task" should {
+      "result BadRequest with Error" in {
+        val Some(result) = route(FakeRequest(GET, s"/task/scalaClass/${new Date().getTime}/${new UUID(1, 1)}"))
+
+        status(result) mustBe SEE_OTHER
+        flash(result).get("flashToUser").get must be("Task does not exist")
       }
     }
   }
