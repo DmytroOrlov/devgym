ea7706746abb9e253eb47cceb41fedf7dda73e19
novakov_alex <novakov.alex@gmail.com>
2016-07-03 11:59:13 +0200

    add help description for each editor on add task form

--- client/src/main/scala/common/CodeEditor.scala
+++ client/src/main/scala/common/CodeEditor.scala
@@ -2,11 +2,13 @@ package common
 
 import scala.scalajs.js
 
-class CodeEditor(editorId: String) {
+class CodeEditor(editorId: String, readOnly: Boolean = false, initialText: String = "") {
   val ace = js.Dynamic.global.ace
   val editor = ace.edit(editorId)
   editor.setTheme("ace/theme/tomorrow")
   editor.getSession().setMode("ace/mode/scala")
+  editor.setReadOnly(readOnly)
+  editor.insert(initialText)
 
   def value = editor.getValue().asInstanceOf[String]
 }
--- client/src/main/scala/controller/AddTaskController.scala
+++ client/src/main/scala/controller/AddTaskController.scala
@@ -6,18 +6,52 @@ import org.scalajs.jquery._
 import scala.scalajs.js.JSApp
 
 object AddTaskController extends JSApp {
+  val solutionTemplateExample =
+    """// if you want another person to implement 'apply' function,
+      |// then just leave its body as empty
+      |class SubArrayWithMaxSum {
+      |  def apply(a: Array[Int]): Array[Int] = {
+      |  // todo: implement ...
+      |  }
+      |}
+      |""".stripMargin
+
+  val suiteExample =
+    """class MinDifferenceInArrayTest(solution: MinDifferenceInArraySolution)
+      |  extends FlatSpec with Matchers {
+      |
+      |  it should "return 0 as min difference in array with non-unique numbers" in {
+      |    solution.minDifference(Array(1, 2, 2, 2, 3, 12)) should be(0)
+      |  }
+      |
+      |  it should "return non 0 as min difference in array with unique numbers" in {
+      |    solution.minDifference(Array(1, 4, 10, 22, 43, 12)) should be(2)
+      |  }
+      |
+      |  it should "return min difference in array with negative numbers" in {
+      |    solution.minDifference(Array(1, 4, 10, 22, 43, -12)) should be(3)
+      |  }
+      |}
+      |
+      |trait MinDifferenceInArraySolution {
+      |  def minDifference(a: Array[Int]): Int
+      |}
+      |""".stripMargin
+
   val buttonId = "submit"
-  var solutionTemplateEditor = new CodeEditor("solutionTemplate")
-  var referenceSolutionEditor = new CodeEditor("referenceSolution")
+  var templateEditor = new CodeEditor("solutionTemplate")
+  var templateEditorExample = new CodeEditor("solutionTemplateExample", readOnly = true, solutionTemplateExample)
+  var referenceEditor = new CodeEditor("referenceSolution")
   var suiteEditor = new CodeEditor("suite")
+  var suiteEditorExample = new CodeEditor("suiteExample", readOnly = true, suiteExample)
 
   def main(): Unit = {
     val submitButton = jQuery(s"#$buttonId")
     submitButton.click(submit _)
 
     def submit() = {
-      jQuery("#solutionTemplate").value(solutionTemplateEditor.value)
-      jQuery("#referenceSolution").value(referenceSolutionEditor.value)
+      jQuery("#solutionTemplate").value(templateEditor.value)
+      jQuery("#referenceSolution").value(referenceEditor.value)
       jQuery("#suite").value(suiteEditor.value)
     }
   }
