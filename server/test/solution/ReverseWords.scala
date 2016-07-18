8c4f59988910a093c7cfd3b8770b29b520ec33f3
novakov_alex <novakov.alex@gmail.com>
2016-07-18 22:30:18 +0200

    add ReverseWordsTest.scala

---
+++ server/test/solution/ReverseWords.scala
@@ -0,0 +1,18 @@
+package solution
+
+import service.PositiveOutput
+import tasktest.{ReverseWordsSolution, ReverseWordsTest}
+
+class TTReverseWords extends PositiveOutput {
+  behavior of "ScalaTestRunner for TTReverseWords"
+
+  override val suiteInstance = new ReverseWordsTest(new ReverseWords)
+}
+
+class ReverseWords extends ReverseWordsSolution {
+  override def reverse(s: String): String = {
+    val separator = " "
+    val words = s.split(separator)
+    words.reverse.filter(!_.isEmpty).mkString(separator)
+  }
+}
