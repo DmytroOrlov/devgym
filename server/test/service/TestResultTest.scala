e8d24ef85560d102577d3dc2e35af37962a05687
novakov_alex <novakov.alex@gmail.com>
2016-08-19 21:23:36 +0200

    add test for testResult function

--- server/test/service/TestResultTest.scala
+++ server/test/service/TestResultTest.scala
@@ -1,7 +1,40 @@
 package service
 
 import org.scalatest.{FlatSpec, Matchers}
+import shared.model.TestStatus
+
+import scala.util.{Failure, Success}
 
 class TestResultTest extends FlatSpec with Matchers {
 
+  it should "return positive test result when test is completely passed" in {
+    //given
+    val report = Success("some report")
+    //when
+    val result = testResult(report)
+    //then
+    result.errorMessage should be (empty)
+    result.status shouldBe TestStatus.Passed.toString
+  }
+
+  it should "return negative test result when report has failed cases" in {
+    //given
+    val report = Success(s"some report is ${service.failed}")
+    //when
+    val result = testResult(report)
+    //then
+    result.errorMessage should be (empty)
+    result.status shouldBe TestStatus.Failed.toString
+  }
+
+  it should "return negative test result when solution is not compilable" in {
+    //given
+    val error = "compilation error"
+    val report = Failure(new RuntimeException(error))
+    //when
+    val result = testResult(report)
+    //then
+    result.errorMessage should be (error)
+    result.status shouldBe TestStatus.Failed.toString
+  }
 }
