8dfc19167265eb28b54fff1b9dcb6fd1aacf8c91
novakov_alex <novakov.alex@gmail.com>
2016-12-31 14:43:27 +0100

    add unit test for CodeParser

--- server/app/service/meta/CodeParser.scala
+++ server/app/service/meta/CodeParser.scala
@@ -3,14 +3,16 @@ package service.meta
 import scala.meta._
 
 object CodeParser {
-  val cr = Term.Name("\r")
-  val lb = Term.Name("\n")
+  private val ls = System.getProperty("line.separator")
+  val lb = Term.Name(ls)
 
   def getSolutionTemplate(code: String): String = {
     val parsed = code.parse[Stat].get
     parsed.transform {
       case q"..$mods def $name[..$tparams](...$paramss): $tpe = $expr" =>
-        q"..$mods def $name[..$tparams](...$paramss): $tpe = {$cr}"
+        q"..$mods def $name[..$tparams](...$paramss): $tpe = {$lb}$lb $lb"
     }.syntax
+      //remove existing line brakes from the passed code string
+      .replace(s"$ls$ls", ls)
   }
 }
