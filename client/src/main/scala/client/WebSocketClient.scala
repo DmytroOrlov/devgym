c60cb955219ceae29b215829207f489e1de43966
Dmytro Orlov <orlov.dm@gmail.com>
2017-01-08 19:57:45 +0100

    closeConnection: revert to () => Unit

--- client/src/main/scala/client/WebSocketClient.scala
+++ client/src/main/scala/client/WebSocketClient.scala
@@ -54,9 +54,9 @@ class WebSocketClient(url: String, timeout: Option[FiniteDuration]) extends Obse
       case NonFatal(e) => Observable.error(e)
     }
 
-    val (inbound, closeConnection: (() => Any)) = try {
+    val (inbound, closeConnection: (() => Unit)) = try {
       val webSocket = new WebSocket(url)
-      inboundWrapper(webSocket) -> (() => if (webSocket.readyState <= 1) Try(webSocket.close()))
+      inboundWrapper(webSocket) -> (() => if (webSocket.readyState <= 1) Try(webSocket.close()): Unit)
     } catch {
       case NonFatal(e) => Observable.error(e) -> (() => ())
     }
